7fc9dce9406710387a3b6bdc418dd7e4e52ea9db
Lyla Lynn <lyladlynn@gmail.com>
2025-02-28 16:36:55 -0600

    created an object with a for of loop. inserted that in to a function and called the function  using  flavorObj

--- index.js
+++ index.js
@@ -1,8 +1,59 @@
 const userInput = prompt (
     "Please enter a list of froyo flavors separated by a comma.", 
-    "Vanilla, vanilla, vanilla, strawberry, coffee, coffee"
+    "vanilla,vanilla,vanilla,strawberry,coffee,coffee"
 );
 
 const froyoFlavor = userInput.split(",")
-console.log(froyoFlavor)
-console.table(froyoFlavor)
+// console.log(froyoFlavor)
+// console.table(froyoFlavor)
+// separating in to 6 separate elements
+
+const flavorObj = froyoOrder(froyoFlavor)
+function froyoOrder (flavorsArr){
+// emplty object assined to flavors
+const flavors = {};
+// next create a loop to go through the array from the userInput
+// need a loop that works with an array
+// best option for this is a for of loop because we are not tracking i as a value
+
+for(const input of flavorsArr){
+    console.log(input)
+// create a variable 
+// "const input" represents the current element of the array
+    if(flavors[input]){
+        flavors[input]=flavors[input] + 1;
+        //flavors[input]+=1 shorter way to write line 21
+    }
+    else{
+        flavors[input]=1;
+    }
+ }
+return flavors;
+}
+console.log(flavorObj)
+console.table(flavorObj)
+
+// // with a function, can call as many times as needed
+// // if not in a function, it will only run one time
+// // emplty object assined to flavors
+// const flavors = {};
+// // next create a loop to go through the array from the userInput
+// // need a loop that works with an array
+// // best option for this is a for of loop because we are not tracking i as a value
+
+// for(const input of froyoFlavor){
+//     console.log(input)
+// // create a variable 
+// // "const input" represents the current element of the array
+//     if(flavors[input]){
+//         flavors[input]=flavors[input] + 1;
+//         //flavors[input]+=1 shorter way to write line 21
+//     }
+//     else{
+//         flavors[input]=1;
+//     }
+// }
+// console.log(flavors)
+// console.table(flavors)
+
+
